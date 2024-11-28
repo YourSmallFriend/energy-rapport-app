710ee9c82d225005d96a8c4fffdb5e5b817d5ed2
YourSmallFriend <SamKint10@hotmail.com>
2024-11-28 10:27:27 +0100

    initial commit eto

---
+++ energy-raport-app/energy-raport-app.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace energy_raport_app.Gtk
+{
+	class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Gtk).Run(new MainForm());
+		}
+	}
+}
